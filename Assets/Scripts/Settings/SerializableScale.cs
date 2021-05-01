d8084bcbaae3cf314e6a23ed71519b0da4f00044
wcremery <whaid.cremery@outlook.fr>
2021-05-01 21:56:01 +0200

    feat(parsing): make Transform serializable

---
+++ Assets/Scripts/Settings/SerializableScale.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Settings
+{
+    [System.Serializable]
+    public class SerializableScale
+    {
+        [SerializeField] private float scaleX;
+        [SerializeField] private float scaleY;
+        [SerializeField] private float scaleZ;
+
+        public SerializableScale(float scaleX, float scaleY, float scaleZ)
+        {
+            this.scaleX = scaleX;
+            this.scaleY = scaleY;
+            this.scaleZ = scaleZ;
+        }
+
+        public Vector3 GetVector3()
+        {
+            return new Vector3(this.scaleX, this.scaleY, this.scaleZ);
+        }
+    }
+}

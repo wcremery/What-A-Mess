bef19ac0a7cab737991ade890adb48d7b948919b
wcremery <whaid.cremery@outlook.fr>
2021-05-02 18:00:13 +0200

    fix(data): add wall class

--- Assets/Scripts/Settings/Wall.cs
+++ Assets/Scripts/Settings/Wall.cs
@@ -6,20 +6,34 @@ using UnityEngine;
 namespace Settings
 {
     [System.Serializable]
-    public class Wall : MonoBehaviour
+    public class Wall
     {
         [SerializeField] private SerializablePosition wallPosition;
         [SerializeField] private SerializableRotation wallRotation;
         [SerializeField] private SerializableScale wallScale;
+        [SerializeField] private bool isColliding;
 
-        private void Awake()
+        public Wall()
+        {
+            wallPosition = new SerializablePosition(0, 0, 0);
+            wallRotation = new SerializableRotation(0, 0, 0, 0);
+            wallScale = new SerializableScale(0, 0, 0);
+
+            isColliding = true;
+        }
+
+        public void Setup(GameObject gameObject)
         {
             Transform transformGO = gameObject.transform;
             Vector3 position = transformGO.position;
             Quaternion rotation = transformGO.rotation;
-            
+            Vector3 scale = transformGO.localScale;
+
             wallPosition = new SerializablePosition(position.x, position.y, position.z);
             wallRotation = new SerializableRotation(rotation.x, rotation.y, rotation.z, rotation.w);
+            wallScale = new SerializableScale(scale.x, scale.y, scale.z);
+
+            isColliding = true;
         }
     }
 }

b51c6542256b032e2b1b802dee996687231dc5fd
wcremery <whaid.cremery@outlook.fr>
2021-05-03 10:55:21 +0200

    feat(player): refactoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,6 @@ public class PlayerController : MonoBehaviour
 
     private float movementSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {

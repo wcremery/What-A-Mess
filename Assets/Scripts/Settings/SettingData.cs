4ae651bb20d47ecfd63b45f273856b4526451e13
wcremery <whaid.cremery@outlook.fr>
2021-05-09 09:45:19 +0200

    fix(project): change data order in json file

--- Assets/Scripts/Settings/SettingData.cs
+++ Assets/Scripts/Settings/SettingData.cs
@@ -8,28 +8,25 @@ namespace Settings
     [System.Serializable]
     public class SettingData
     {
-        [SerializeField] private Wall[] walls;
         [SerializeField] private Player player;
         [SerializeField] private Interaction[] interactions;
         [SerializeField] private Item[] items;
+        [SerializeField] private Wall[] walls;
         
         public SettingData()
         {
-            this.walls = new Wall[4];
             this.player = new Player();
             this.interactions = new Interaction[1];
             this.items = new Item[1];
+            this.walls = new Wall[4];
         }
-
-        public Wall[] Walls => walls;
 
         public Player Player => player;
 
         public Interaction[] Interactions => interactions;
 
         public Item[] Items => items;
+        
+        public Wall[] Walls => walls;
     }
-    
-    
-    
 }
